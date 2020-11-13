a026415f027278415b410b142161ed26992451b8
leandroflores <leandroflores7@gmail.com>
2020-11-13 00:16:50 -0200

    - Commit Inicial.

---
+++ Grafo/src/modelo/grafo/matriz/Aresta.java
@@ -0,0 +1,15 @@
+package modelo.grafo.matriz;
+
+/**
+ * <p>Classe <b>Aresta</b>.</p>
+ * <p>Classe responsavel por representar as <b>Arestas</b> do <b>Grafo</b>.</p>
+ * @author Leandro
+ * @since  12/11/2020
+ */
+public class Aresta {
+    private String  origem;
+    private String  destino;
+    private Integer peso;
+    
+    
+}
